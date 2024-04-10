f4d23e5a12cca4e0586323ac19240b44dad2c1c3
Todd Goates <toddgoates@gmail.com>
2024-04-10 09:36:02 -0600

    Update Emphasis component styling

--- app/components/Emphasis.tsx
+++ app/components/Emphasis.tsx
@@ -1,5 +1,3 @@
 export default function Emphasis({ children }: { children: React.ReactNode }) {
-  return (
-    <span className="border-b-4 border-dashed border-blue-600">{children}</span>
-  );
+  return <span className="border-b-4 border-blue-600">{children}</span>;
 }
